6ad297a5a5bec0794d0ce42b476746ffee01a401
Haz-git <zhouharry1@gmail.com>
2021-01-05 08:52:57 -0800

    Connected server to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 //Grabbing App:
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app.js');
+
+//Using dotenv for env variables:
+dotenv.config({
+    path: `${__dirname}/config.env`,
+});
 
 //Initializing PORT:
 const PORT = 7467;
 
 //Creating server
 const server = require('http').createServer(app);
+
+//Connecting app to MongoDB Atlas via Mongoose:
+const connectToDb = mongoose
+    .connect(
+        process.env.DATABASE.replace(
+            '<PASSWORD>',
+            process.env.DATABASE_PASSWORD
+        ),
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        }
+    )
+    .then(() => {
+        console.log(`Connection to MongoDB Atlas established successfully.`);
+    });
 
 //Server Start:
 server.listen(PORT, () => {
