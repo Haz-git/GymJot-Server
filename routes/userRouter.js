0da3284e78693c2e3bc6f0f885707f4bfb2da271
Haz-git <zhouharry1@gmail.com>
2021-01-06 09:25:01 -0800

    Added login, registration, and protected test route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,10 +2,22 @@
 const express = require('express');
 const router = express.Router();
 
+//Passport:
+const passport = require('passport');
+
 //Controllers:
 const authController = require('../controllers/authControllers');
 
 //Authentication Routes:
-router.route('/signup').get(authController.testController);
+router.route('/signup').post(authController.signup);
+router.route('/login').post(authController.login);
+
+//Test protected route:
+router
+    .route('/protected')
+    .get(
+        passport.authenticate('jwt', { session: false }),
+        authController.testController
+    );
 
 module.exports = router;
