de2932c026a1d9ebdca554143b23fa80d8a4fbd0
Haz-git <zhouharry1@gmail.com>
2021-01-06 09:25:41 -0800

    Added handleAsync to remove try/catch DRY

--- util/handleAsync.js
+++ util/handleAsync.js
@@ -1,3 +1,6 @@
+//Preventing the need of a try/catch block with every request handler.
+//The .catch portion catches any errors that tosses it to the error handler.
+
 module.exports = (fn) => {
     return (req, res, next) => {
         fn(req, res, next).catch(next);
