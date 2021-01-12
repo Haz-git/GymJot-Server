fa6d6792489cd95413ebd54ee26eb8fc44d265c8
Haz-git <zhouharry1@gmail.com>
2021-01-12 08:38:51 -0800

    Adding new route for userDetails

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,17 +7,26 @@ const passport = require('passport');
 
 //Controllers:
 const authController = require('../controllers/authControllers');
+const userDetailControllers = require('../controllers/userDetailControllers');
 
 //Authentication Routes:
 router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
 
-//Test protected route:
+//Test protected route: -- We are testing JWT expiration and access.
 router
     .route('/protected')
     .get(
         passport.authenticate('jwt', { session: false }),
         authController.testController
+    );
+
+//Initial Route to load user data:
+router
+    .route('/getUserData')
+    .get(
+        passport.authenticate('jwt', { session: false }),
+        userDetailControllers.getUserData
     );
 
 module.exports = router;
