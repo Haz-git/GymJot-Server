ecc22990245ddc6f3ad2cfad1e53b020343878c2
Haz-git <zhouharry1@gmail.com>
2021-01-12 11:39:23 -0800

    Enabled body parser and removed userDetails routers

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,7 +7,6 @@ const passport = require('passport');
 
 //Controllers:
 const authController = require('../controllers/authControllers');
-const userDetailControllers = require('../controllers/userDetailControllers');
 
 //Authentication Routes:
 router.route('/signup').post(authController.signup);
@@ -19,14 +18,6 @@ router
     .get(
         passport.authenticate('jwt', { session: false }),
         authController.testController
-    );
-
-//Initial Route to load user data:
-router
-    .route('/getUserData')
-    .get(
-        passport.authenticate('jwt', { session: false }),
-        userDetailControllers.getUserData
     );
 
 module.exports = router;
